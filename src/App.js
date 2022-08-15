329cec971966ee2ecd3022d9436aeb838ad91f32
valyndsilva <valyndsilva@gmail.com>
2022-08-15 13:01:20 +0100

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+
+// Action object lookup and dot notation recommeneded by Dan Abramov
+const ACTION = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+  NEW_USER_INPUT: "newUserInput",
+  TG_COLOR: "tgColor",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION.INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case ACTION.DECREMENT:
+      return { ...state, count: state.count - 1 };
+    case ACTION.NEW_USER_INPUT:
+      return { ...state, userInput: action.payload };
+    case ACTION.TG_COLOR:
+      return { ...state, color: !state.color };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  // const [count, setCount] = useState(0);
+  // const [userInput, setUserInput] = useState("");
+  // const [color, setColor] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    userInput: "",
+    color: false,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App" style={{ color: state.color ? "#FFF" : "#FFF952" }}>
+      <input
+        type="text"
+        value={state.userInput}
+        onChange={(e) =>
+          dispatch({ type: ACTION.NEW_USER_INPUT, payload: e.target.value })
+        }
+      />
+      <br />
+      <br />
+      <p>{state.count}</p>
+      <section>
+        {/* <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+        <button onClick={() => setColor((prev) => !prev)}>Color</button> */}
+        <button onClick={() => dispatch({ type: ACTION.DECREMENT })}>-</button>
+        <button onClick={() => dispatch({ type: ACTION.INCREMENT })}>+</button>
+        <button onClick={() => dispatch({ type: ACTION.TG_COLOR })}>
+          Color
+        </button>
+      </section>
+      <br />
+      <br />
+      <p>{state.userInput}</p>
+    </main>
   );
 }
 
